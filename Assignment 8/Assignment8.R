022f805ef86e3932d96c7cf5e8362021c4ec6325
Fauzan A <67179349+acetlinx@users.noreply.github.com>
2021-04-04 05:39:18 -0400

    Add files via upload

--- Assignment 8/Assignment8.R
+++ Assignment 8/Assignment8.R
@@ -4,19 +4,37 @@ install.packages("rpart")
 install.packages("rpart.plot")
 library(rpart)
 library(rpart.plot)
+M2021test_students <- read_csv("Desktop/Data101 Assignment 8/M2021test-students.csv")
+M2021test_submission_file <- read_csv("Desktop/Data101 Assignment 8/M2021test-submission-file.csv")
 
 colors = c("red", "green", "blue")
 
 ## Summary
 summary(trainData)
-myprediction<-trainData
+myprediction <- M2021test_students
 decision <- rep('Fail',nrow(myprediction))
-decision[myprediction$Score>50] <- 'Pass'
-decision[myprediction$Score<50] <- 'Fail'
+decision[myprediction$Score>=50] <- 'Pass'
+decision[myprediction$Score<50 ] <- 'Fail'
+## First Half
+decision[myprediction$Score<50 & (myprediction$Seniority == "Freshman" | myprediction$Seniority == "Junior") & myprediction$Score > 39.5 & myprediction$Attendance >= 59.5] <- 'Pass' ##  0.1781488
+decision[myprediction$Score<50 & (myprediction$Major != "Cs") & myprediction$Questions == "Always" & myprediction$Major == "Polsci"] <- 'Pass' ## error -> 0.1843829
+decision[myprediction$Score<50 & (myprediction$Major != "Cs") & myprediction$Questions == "Always" & myprediction$Major != "Polsci" & myprediction$Score > 40.5] <- 'Pass' ## 0.1749789
+decision[myprediction$Score<50 & (myprediction$Major != "Cs") & myprediction$Questions == "Rarely" & myprediction$Score >= 38.8] <- 'Pass' ## 0.1785714
+## Second Half
+decision[myprediction$Score >= 50 & myprediction$Score < 69.5 & myprediction$Major == "Cs" & (myprediction$Seniority == "Senior" | myprediction$Seniority == "Junior")] <- 'Fail'
+## input here
 myprediction$Grade <-decision
-trainData$Projection <-decision
-error <- mean(trainData$Grade != myprediction$Grade)
-error
+M2021test_students$Projection <-decision
+
+
+## Running on Test File
+Studentid <- c(M2021test_students$Studentid)
+Studentid
+Grade<- c(M2021test_students$Projection)
+Grade
+df <- data.frame(Studentid,Grade)
+df
+write.csv(df, "Desktop/Fauzan_Real_Submission.csv", row.names = FALSE)
 
 ## Plots of Values Against Grade -> Rough Overview
 boxplot(trainData$Attendance~trainData$Grade, main = "Attendance VS Grade", xlab = "Grade", ylab = "Attendance", col = c("red", "green", "blue"))
@@ -44,7 +62,7 @@ subsetMajorSmall <- subset(subsetSmall, Major != "Cs")
 
 ## CS and Score < 50
 boxplot(subsetCSsmall$Attendance~subsetCSsmall$Grade, main = "Attendance VS Grade for CS Students w/ Score < 50", xlab = "Grade", ylab = "Attendance", col = c("red", "green", "blue"))
-mosaicplot(ssubsetCSsmall$Major~subsetCSsmall$Grade, main = "Major VS Grade for CS Students w/ Score < 50", xlab = "Major", ylab = "Grade", col = c("red", "green", "blue"))
+mosaicplot(subsetCSsmall$Major~subsetCSsmall$Grade, main = "Major VS Grade for CS Students w/ Score < 50", xlab = "Major", ylab = "Grade", col = c("red", "green", "blue"))
 mosaicplot(subsetCSsmall$Questions~subsetCSsmall$Grade, main = "Questions VS Grade for CS Students w/ Score < 50", xlab = "Questions", ylab = "Grade", col = c("red", "green", "blue"))
 boxplot(subsetCSsmall$Score~subsetCSsmall$Grade, main = "Score VS Grade for CS Students w/ Score < 50", xlab = "Grade", ylab = "Score", col = c("red", "green", "blue"))
 mosaicplot(subsetCSsmall$Seniority~subsetCSsmall$Grade, main = "Seniority VS Grade for CS Students w/ Score < 50", xlab = "Seniority", ylab = "Grade", col = c("red", "green", "blue"))
@@ -53,10 +71,30 @@ mosaicplot(subsetCSsmall$Texting~subsetCSsmall$Grade, main = "Texting VS Grade f
 ## Other Majors and Score < 50
 boxplot(subsetMajorSmall$Attendance~subsetMajorSmall$Grade, main = "Attendance VS Grade for Non-CS Students w/ Score < 50", xlab = "Grade", ylab = "Attendance", col = c("red", "green", "blue"))
 mosaicplot(subsetMajorSmall$Major~subsetMajorSmall$Grade, main = "Major VS Grade for Non-CS Students w/ Score < 50", xlab = "Major", ylab = "Grade", col = c("red", "green", "blue"))
-mosaicplot(ssubsetMajorSmall$Questions~subsetMajorSmall$Grade, main = "Questions VS Grade for Non-CS Students w/ Score < 50", xlab = "Questions", ylab = "Grade", col = c("red", "green", "blue"))
+mosaicplot(subsetMajorSmall$Questions~subsetMajorSmall$Grade, main = "Questions VS Grade for Non-CS Students w/ Score < 50", xlab = "Questions", ylab = "Grade", col = c("red", "green", "blue"))
 boxplot(subsetMajorSmall$Score~subsetMajorSmall$Grade, main = "Score VS Grade for Non-CS Students w/ Score < 50", xlab = "Grade", ylab = "Score", col = c("red", "green", "blue"))
 mosaicplot(subsetMajorSmall$Seniority~subsetMajorSmall$Grade, main = "Seniority VS Grade for Non-CS Students w/ Score < 50", xlab = "Seniority", ylab = "Grade", col = c("red", "green", "blue"))
 mosaicplot(subsetMajorSmall$Texting~subsetMajorSmall$Grade, main = "Texting VS Grade for Non-CS Students w/ Score < 50", xlab = "Texting", ylab = "Grade", col = c("red", "green", "blue"))
+
+## Splitting Non Cs student w/ Score < 50 by Questions
+subsetMajorSmallAlways <- subset(subsetMajorSmall, Questions == "Always")
+subsetMajorSmallRarely <- subset(subsetMajorSmall, Questions == "Rarely")
+
+## Non Cs Students Score < 50 Always Ask Questions Plots
+boxplot(subsetMajorSmallAlways$Attendance~subsetMajorSmallAlways$Grade, main = "Attendance VS Grade for Non-CS Always Question-Asking Students w/ Score < 50", xlab = "Grade", ylab = "Attendance", col = c("red", "green", "blue"))
+mosaicplot(subsetMajorSmallAlways$Major~subsetMajorSmallAlways$Grade, main = "Major VS Grade for Non-CS Always Question-Asking Students w/ Score < 50", xlab = "Major", ylab = "Grade", col = c("red", "green", "blue"))
+mosaicplot(subsetMajorSmallAlways$Questions~subsetMajorSmallAlways$Grade, main = "Questions VS Grade for Non-CS Always Question-Asking Students w/ Score < 50", xlab = "Questions", ylab = "Grade", col = c("red", "green", "blue"))
+boxplot(subsetMajorSmallAlways$Score~subsetMajorSmallAlways$Grade, main = "Score VS Grade for Non-CS Always Question-Asking Students w/ Score < 50", xlab = "Grade", ylab = "Score", col = c("red", "green", "blue"))
+mosaicplot(subsetMajorSmallAlways$Seniority~subsetMajorSmallAlways$Grade, main = "Seniority VS Grade for Non-CS Always Question-Asking Students w/ Score < 50", xlab = "Seniority", ylab = "Grade", col = c("red", "green", "blue"))
+mosaicplot(subsetMajorSmallAlways$Texting~subsetMajorSmallAlways$Grade, main = "Texting VS Grade for Non-CS Always Question-Asking Students w/ Score < 50", xlab = "Texting", ylab = "Grade", col = c("red", "green", "blue"))
+
+## Non Cs Students Score < 50 Rarely Ask Questions
+boxplot(subsetMajorSmallRarely$Attendance~subsetMajorSmallRarely$Grade, main = "Attendance VS Grade for Non-CS Rarely Question-Asking Students w/ Score < 50", xlab = "Grade", ylab = "Attendance", col = c("red", "green", "blue"))
+mosaicplot(subsetMajorSmallRarely$Major~subsetMajorSmallRarely$Grade, main = "Major VS Grade for Non-CS Rarely Question-Asking Students w/ Score < 50", xlab = "Major", ylab = "Grade", col = c("red", "green", "blue"))
+mosaicplot(subsetMajorSmallRarely$Questions~subsetMajorSmallRarely$Grade, main = "Questions VS Grade for Non-CS Rarely Question-Asking Students w/ Score < 50", xlab = "Questions", ylab = "Grade", col = c("red", "green", "blue"))
+boxplot(subsetMajorSmallRarely$Score~subsetMajorSmallRarely$Grade, main = "Score VS Grade for Non-CS Rarely Question-Asking Students w/ Score < 50", xlab = "Grade", ylab = "Score", col = c("red", "green", "blue"))
+mosaicplot(subsetMajorSmallRarely$Seniority~subsetMajorSmallRarely$Grade, main = "Seniority VS Grade for Non-CS Rarely Question-Asking Students w/ Score < 50", xlab = "Seniority", ylab = "Grade", col = c("red", "green", "blue"))
+mosaicplot(subsetMajorSmallRarely$Texting~subsetMajorSmallRarely$Grade, main = "Texting VS Grade for Non-CS Rarely Question-Asking Students w/ Score < 50", xlab = "Texting", ylab = "Grade", col = c("red", "green", "blue"))
 
 ## Analyzing SubsetBig
 boxplot(subsetBig$Attendance~subsetBig$Grade, main = "Attendance VS Grade for Score >= 50", xlab = "Grade", ylab = "Attendance", col = c("red", "green", "blue"))
@@ -65,3 +103,36 @@ mosaicplot(subsetBig$Questions~subsetBig$Grade, main = "Questions VS Grade for S
 boxplot(subsetBig$Score~subsetBig$Grade, main = "Score VS Grade for Score  >= 50", xlab = "Grade", ylab = "Score", col = c("red", "green", "blue"))
 mosaicplot(subsetBig$Seniority~subsetBig$Grade, main = "Seniority VS Grade for Score  >= 50", xlab = "Seniority", ylab = "Grade", col = c("red", "green", "blue"))
 mosaicplot(subsetBig$Texting~subsetBig$Grade, main = "Texting VS Grade for Score  >= 50", xlab = "Texting", ylab = "Grade", col = c("red", "green", "blue"))
+
+## 69 Big
+subsetBigSmall <- subset(subsetBig,Score < 69 )
+subsetBigBig <- subset(subsetBig,Score >= 69 )
+
+## Analyzing SubsetBigSmall
+boxplot(subsetBigSmall$Attendance~subsetBigSmall$Grade, main = "Attendance VS Grade for Score >= 50 and < 69", xlab = "Grade", ylab = "Attendance", col = c("red", "green", "blue"))
+mosaicplot(subsetBigSmall$Major~subsetBigSmall$Grade, main = "Major VS Grade for Score  >= 50 and < 69", xlab = "Major", ylab = "Grade", col = c("red", "green", "blue"))
+mosaicplot(subsetBigSmall$Questions~subsetBigSmall$Grade, main = "Questions VS Grade for Score  >= 50 and < 69", xlab = "Questions", ylab = "Grade", col = c("red", "green", "blue"))
+boxplot(subsetBigSmall$Score~subsetBigSmall$Grade, main = "Score VS Grade for Score  >= 50 and < 69", xlab = "Grade", ylab = "Score", col = c("red", "green", "blue"))
+mosaicplot(subsetBigSmall$Seniority~subsetBigSmall$Grade, main = "Seniority VS Grade for Score  >= 50 and < 69", xlab = "Seniority", ylab = "Grade", col = c("red", "green", "blue"))
+mosaicplot(subsetBigSmall$Texting~subsetBigSmall$Grade, main = "Texting VS Grade for Score  >= 50 and < 69", xlab = "Texting", ylab = "Grade", col = c("red", "green", "blue"))
+
+## SubsetBigSmall CS
+subsetBigSmallCS <- subset(subsetBigSmall,Major == "Cs" )
+boxplot(subsetBigSmallCS$Attendance~subsetBigSmallCS$Grade, main = "Attendance VS Grade for CS Student who Score >= 50 and < 69", xlab = "Grade", ylab = "Attendance", col = c("red", "green", "blue"))
+mosaicplot(subsetBigSmallCS$Major~subsetBigSmallCS$Grade, main = "Major VS Grade for CS Student who Score  >= 50 and < 69", xlab = "Major", ylab = "Grade", col = c("red", "green", "blue"))
+mosaicplot(subsetBigSmallCS$Questions~subsetBigSmallCS$Grade, main = "Questions VS Grade forCS Student who Score  >= 50 and < 69", xlab = "Questions", ylab = "Grade", col = c("red", "green", "blue"))
+boxplot(subsetBigSmallCS$Score~subsetBigSmallCS$Grade, main = "Score VS Grade for for CS Student who Score  >= 50 and < 69", xlab = "Grade", ylab = "Score", col = c("red", "green", "blue"))
+mosaicplot(subsetBigSmallCS$Seniority~subsetBigSmallCS$Grade, main = "Seniority VS Grade for CS Student who Score  >= 50 and < 69", xlab = "Seniority", ylab = "Grade", col = c("red", "green", "blue"))
+mosaicplot(subsetBigSmallCS$Texting~subsetBigSmallCS$Grade, main = "Texting VS Grade for CS Student who Score  >= 50 and < 69", xlab = "Texting", ylab = "Grade", col = c("red", "green", "blue"))
+
+## Analyzing SubsetBigBig
+boxplot(subsetBigBig$Attendance~subsetBigBig$Grade, main = "Attendance VS Grade for Score >= 69", xlab = "Grade", ylab = "Attendance", col = c("red", "green", "blue"))
+mosaicplot(subsetBigBig$Major~subsetBigBig$Grade, main = "Major VS Grade for Score  >= 69", xlab = "Major", ylab = "Grade", col = c("red", "green", "blue"))
+mosaicplot(subsetBigBig$Questions~subsetBigBig$Grade, main = "Questions VS Grade for Score  >= 69", xlab = "Questions", ylab = "Grade", col = c("red", "green", "blue"))
+boxplot(subsetBigBig$Score~subsetBigBig$Grade, main = "Score VS Grade for Score  >= 69", xlab = "Grade", ylab = "Score", col = c("red", "green", "blue"))
+mosaicplot(subsetBigBig$Seniority~subsetBigBig$Grade, main = "Seniority VS Grade for Score  >= 69", xlab = "Seniority", ylab = "Grade", col = c("red", "green", "blue"))
+mosaicplot(subsetBigBig$Texting~subsetBigBig$Grade, main = "Texting VS Grade for Score  >= 69", xlab = "Texting", ylab = "Grade", col = c("red", "green", "blue"))
+
+
+
+
